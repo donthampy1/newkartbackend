6935a6ae0491902681836ebac3249635df062f9e
donthampy1 <donthampyvalamkottil@gmail.com>
2024-09-10 02:45:48 +0530

    added stripe webhook

--- routes/webhookRouter.js
+++ routes/webhookRouter.js
@@ -3,9 +3,13 @@ const router = express.Router()
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const OrderDetails = require ('../models/orderDetailsModel')
+const  model = require ('../models/usermodel.js')
+const User = model.User
+const CartData = require('../models/cartModel') 
 
 
-router.post('/', express.raw({ type: 'application/json' }), (req, res) => {
+
+router.post('/', express.raw({ type: 'application/json' }), async (req, res) => {
     const sig = req.headers['stripe-signature'];
     let event;
 
@@ -29,8 +33,59 @@ router.post('/', express.raw({ type: 'application/json' }), (req, res) => {
             console.log(`Unhandled event type ${event.type}`);
     }
 
-    // Return a response to acknowledge receipt of the event
-    res.json({ received: true });
+
+
+
+
+
+    if (event.type === 'checkout.session.completed') {
+        const session = event.data.object;
+    
+        try {
+          // Find the order by paymentId and update the status
+          const order = await  OrderDetails.findOne({ paymentId: session.id });
+    
+          if (order) {
+            order.paymentStatus = 'Completed';
+            await order.save();
+            console.log(`Order ${order._id} payment status updated to 'Completed'`);
+          }
+          const email = session.customer_details.email
+          const user = await User.findOne({ email: email })
+          const userId = user._id
+          const result = await CartData.deleteOne({ userId: userId });
+
+
+
+
+
+
+    
+        } catch (error) {
+          console.error('Error updating order status:', error);
+          res.status(500).send('Server Error');
+        }
+      } else {
+        // Return a 200 response for any other event
+        res.status(200).send({ received: true });
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 });
 
 exports.router = router
